661e33a0603a9337b883d50035f5f98face8a368
Abdullatif <abdullatiffarah21@gmail.com>
2022-06-21 16:00:00 +0100

    I made 2 components for the lower section  the quote and the wellbeing button.

--- power-coders-app/src/Components/App/App.js
+++ power-coders-app/src/Components/App/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import Quotes from '../LowerSection/Quote/quote';
+import WellBeing from '../LowerSection/WellBeingButton/wellBeingButton';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+
+      <main>
+
+        <section className='LowerSection'>
+          <Quotes  author="Abdullatif" quotes="Growth is internal, changes aren't noticed till too late."></Quotes>
+          <WellBeing></WellBeing>
+        </section>
+        
+      </main>
     </div>
   );
 }
---
+++ power-coders-app/src/Components/LowerSection/WellBeingButton/wellBeingButton.js
@@ -0,0 +1,8 @@
+import React from "react"
+
+
+function WellBeing (){
+    return <button>Well being <br></br>Check-In</button>
+}
+
+export default WellBeing
