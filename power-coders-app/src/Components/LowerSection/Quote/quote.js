633596af8c82ebdaffd22ff1d6021688121662b4
cragglesummers <73234010+AlphaPentagon@users.noreply.github.com>
2022-06-22 17:18:11 +0100

    Created randomQuote fetch function - not working

--- power-coders-app/src/Components/LowerSection/Quote/quote.js
+++ power-coders-app/src/Components/LowerSection/Quote/quote.js
@@ -2,15 +2,34 @@
 //import react from "react"
 //import css from "./quotes.css"
 
-function Quotes(props) {
+import react, { useState, useEffect } from "react";
+
+function Quotes() {
+    const [quote, setQuote] = useState([]);
+    async function randomQuote() {
+        const response = await fetch('https://zenquotes.io/api/random', {
+            method: "GET",
+            mode: 'no-cors'
+        }); 
+        const data = await response.json();
+        console.log(data)
+                    }
+    randomQuote();
+
+    useEffect(() => {
+        
+    },[])
+
     return ([
         <section>        
-            <h2 >{props.quotes}</h2>
-            <h3>-{props.author}</h3>
+            <h2 >{quote.q}</h2>
+            <h3>-{quote.a}</h3>
         </section>
 
     ]
     )
 }
+
+
 
 export default Quotes; 
